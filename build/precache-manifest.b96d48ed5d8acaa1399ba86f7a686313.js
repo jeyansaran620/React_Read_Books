5469744a916f8072618e7454bb1312127c2bb2b1
jeyansaran620 <jeyansaran620@gmail.com>
2020-05-16 11:39:17 +0530

    test changed

---
+++ build/precache-manifest.b96d48ed5d8acaa1399ba86f7a686313.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5b03bcb6e9af8cdb3e3890465ac867",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83d2b99dbb290301ea47",
+    "url": "/static/css/main.db5b55ce.chunk.css"
+  },
+  {
+    "revision": "5182004b845624114e2b",
+    "url": "/static/js/2.c35d0bce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c35d0bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d2b99dbb290301ea47",
+    "url": "/static/js/main.e9c47b5e.chunk.js"
+  },
+  {
+    "revision": "bcb0f5c46daff1a202f5",
+    "url": "/static/js/runtime-main.8d42c8df.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
